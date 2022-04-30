282fd4c2f3e9d685e44db22d3e0e8cae957dabf4
SeongMin-K <eo1tks3@naver.com>
2022-05-01 02:51:23 +0900

    Toggle Front/Back Camera

--- LocalCameraMetal_Swift/ViewController.swift
+++ LocalCameraMetal_Swift/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var preview: UIImageView!
+    @IBOutlet weak var cameraToggleBtn: UIButton!
+    
+    var captureSession: AVCaptureSession!
+    var backCameraInput: AVCaptureDeviceInput!
+    var frontCameraInput: AVCaptureDeviceInput!
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        setupLivePreview()
+        
+        guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else { return }
+        guard let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else { return }
+        
+        do {
+            backCameraInput = try AVCaptureDeviceInput(device: backCamera)
+            frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
+            
+            let photoOutput = AVCapturePhotoOutput()
+            photoOutput.isHighResolutionCaptureEnabled = true
+            
+            if captureSession.canAddInput(backCameraInput), captureSession.canAddOutput(photoOutput) {
+                captureSession.addInput(backCameraInput)
+                captureSession.addOutput(photoOutput)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+
+        startCaptureSession()
     }
-
-
+    
+    private func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        
+        preview.layer.insertSublayer(videoPreviewLayer, at: 0)
+        DispatchQueue.main.async {
+            self.videoPreviewLayer.frame = self.preview.bounds
+        }
+    }
+    
+    private func startCaptureSession() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    @IBAction func onCameraToggleBtnClicked(_ sender: UIButton) {
+        captureSession.beginConfiguration()
+        
+        if captureSession.inputs[0] == backCameraInput {
+            captureSession.removeInput(backCameraInput)
+            captureSession.addInput(frontCameraInput)
+        } else if captureSession.inputs[0] == frontCameraInput {
+            captureSession.removeInput(frontCameraInput)
+            captureSession.addInput(backCameraInput)
+        }
+        
+        captureSession.commitConfiguration()
+    }
 }
-
